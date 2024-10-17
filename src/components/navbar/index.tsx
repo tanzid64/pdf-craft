42efa054ecc234d5729272a176a471fd590b04ce
tanzid64 <tanzid@inbox.ru>
2024-10-18 01:36:37 +0600

    fix: navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,14 +1,14 @@
-"use client";
 import { credentialsSignOut } from "@/action/auth";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { FC } from "react";
 import { MaxWidthWrapper } from "../global/max-width-wrapper";
 import { Button, buttonVariants } from "../ui/button";
+import { auth } from "@/auth";
 
-export const Navbar: FC = () => {
-  const session = useSession();
-  const user = session.data?.user;
+export const Navbar: FC = async () => {
+  const session = await auth();
+  const user = session?.user;
   return (
     <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
@@ -16,44 +16,56 @@ export const Navbar: FC = () => {
           <Link href="/" className="flex z-40 font-semibold">
             <span>quill.</span>
           </Link>
-          {/* TODO add mobile navbar */}
-          <div className="hidden items-center space-x-4 sm:flex">
-            <>
-              <Link
-                href="/pricing"
-                className={buttonVariants({
-                  variant: "ghost",
-                  size: "sm",
-                })}
-              >
-                Pricing
-              </Link>
 
-              <Link
-                href="/auth/sign-in"
-                className={buttonVariants({
-                  variant: "outline",
-                  size: "sm",
-                })}
-              >
-                Login
-              </Link>
-              <Link
-                href="/auth/sign-up"
-                className={buttonVariants({
-                  size: "sm",
-                })}
-              >
-                Get Started
-              </Link>
-              {user && (
+          {/* Mobile Nav */}
+          <div className="hidden items-center space-x-4 sm:flex">
+            {!user ? (
+              <>
+                <Link
+                  href="/pricing"
+                  className={buttonVariants({
+                    variant: "ghost",
+                    size: "sm",
+                  })}
+                >
+                  Pricing
+                </Link>
+                <Link
+                  href="/auth/sign-in"
+                  className={buttonVariants({
+                    variant: "outline",
+                    size: "sm",
+                  })}
+                >
+                  Login
+                </Link>
+                <Link
+                  href="/auth/sign-up"
+                  className={buttonVariants({
+                    size: "sm",
+                  })}
+                >
+                  Get Started
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/dashboard"
+                  className={buttonVariants({
+                    variant: "ghost",
+                    size: "sm",
+                  })}
+                >
+                  Dashboard
+                </Link>
                 <form action={credentialsSignOut}>
                   <Button variant={"outline"} size={"sm"} type="submit">
                     Logout
                   </Button>
                 </form>
-              )}
-            </>
+              </>
+            )}
           </div>
         </div>
       </MaxWidthWrapper>
