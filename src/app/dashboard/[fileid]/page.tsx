df2095d57fcbb535a9b9d0f60b70a4117659d1ec
tanzid64 <tanzid@inbox.ru>
2024-10-19 03:47:29 +0600

    TODO: Complete PDF Renderer component & create custom hook

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -27,13 +27,12 @@ const FilePage: FC<FilePageProps> = async ({ params }) => {
         <div className="flex-1 xl:flex">
           <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
             {/* Main area */}
-            <PdfRenderer url={file?.url!} />
+            <PdfRenderer url={file.url} />
           </div>
         </div>
 
-        {/* Right sidebar & Chat wrapper */}
         <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
-          <ChatWrapper fileId={file.url} />
+          <ChatWrapper fileId={file.id} />
         </div>
       </div>
     </div>
--- src/components/pdf-renderer/index.tsx
+++ src/components/pdf-renderer/index.tsx
@@ -1,11 +1,18 @@
 "use client";
-import { Loader2 } from "lucide-react";
-import { FC } from "react";
+import { cn } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ChevronDown, ChevronUp, Loader2, Search } from "lucide-react";
+import { FC, useState } from "react";
+import { useForm } from "react-hook-form";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
+import { useResizeDetector } from "react-resize-detector";
 import { toast } from "sonner";
-
+import { z } from "zod";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
 // worker to render pdf
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.mjs`;
 
@@ -14,15 +21,103 @@ interface PdfRendererProps {
 }
 
 export const PdfRenderer: FC<PdfRendererProps> = ({ url }) => {
+  const { width, ref } = useResizeDetector();
+  const [numPages, setNumPages] = useState<number | undefined>(undefined);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  // Handle Input page
+  const customPageValidator = z.object({
+    page: z.string().refine((page) => {
+      Number(page) > 0 && Number(page) <= numPages!;
+    }),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm<z.infer<typeof customPageValidator>>({
+    defaultValues: {
+      page: "1",
+    },
+    resolver: zodResolver(customPageValidator),
+  });
+
+  const handlePageSubmit =
+    () =>
+    ({ page }: z.infer<typeof customPageValidator>) => {
+      setCurrentPage(Number(page));
+      setValue("page", String(page));
+    };
+
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
         {/* Top bar */}
-        <div className="flex items-center gap-1.5"></div>
+        <div className="flex items-center gap-1.5">
+          <Button
+            aria-label="previous page"
+            variant={"ghost"}
+            onClick={() =>
+              setCurrentPage((prev) => (prev - 1 > 1 ? prev - 1 : 1))
+            }
+            disabled={currentPage <= 1}
+          >
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+          <div className="flex items-center gap-1.5">
+            <Input
+              {...register("page")}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleSubmit(handlePageSubmit)();
+                }
+              }}
+              className={cn(
+                "w-12 h-8",
+                errors.page && "focus-visible:ring-red-500",
+              )}
+            />
+            <p className="text-sm text-zinc-700 space-x-1">
+              <span>/</span>
+              <span>{numPages ?? "x"}</span>
+            </p>
+          </div>
+
+          <Button
+            aria-label="next page"
+            variant={"ghost"}
+            onClick={() =>
+              setCurrentPage((prev) =>
+                prev + 1 < numPages! ? prev + 1 : numPages!,
+              )
+            }
+            disabled={currentPage === numPages || numPages === undefined}
+          >
+            <ChevronUp className="h-4 w-4" />
+          </Button>
+        </div>
+        <div className="space-x-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button aria-label="zoom" variant={"ghost"} className="gap-1.5">
+                <Search className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>
+                100%
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
       <div className="flex-1 w-full max-h-screen">
-        <div>
+        <div ref={ref}>
           <Document
+            onLoadSuccess={({ numPages }) => {
+              setNumPages(numPages);
+            }}
             loading={
               <div className="flex justify-center">
                 <Loader2 className="animate-spin my-24 h-6 w-6" />
@@ -36,7 +131,7 @@ export const PdfRenderer: FC<PdfRendererProps> = ({ url }) => {
             file={url}
             className="max-h-full"
           >
-            <Page pageNumber={1} renderAnnotationLayer={false} />
+            <Page width={width ? width : 1} pageNumber={currentPage} />
           </Document>
         </div>
       </div>
